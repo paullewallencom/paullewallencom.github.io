9be446bce74ba363dac250fa1c0cc45943040764
Paul Lewallen <paul.lewallen@gmail.com>
2018-11-12 13:59:15 -0800

    image

--- 008-Loading-images-offline/service-worker.js
+++ 008-Loading-images-offline/service-worker.js
@@ -11,7 +11,7 @@ function installHandler(event) {
         caches.open(cacheName).then(function(cache) {
             return cache.addAll([
               'index.html',
-              'packt-logo.png'
+              'logo.jpg'
             ]);
         })
     );

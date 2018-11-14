3fdce2e0654ef811a2a58a83c9996685afe00fe3
Paul Lewallen <paul.lewallen@gmail.com>
2018-11-13 17:12:54 -0800

    034-Implementing-a-circuit-breaker

---
+++ 034-Implementing-a-circuit-breaker/service-worker.js
@@ -0,0 +1,55 @@
+importScripts('circuit-breaker.js');
+
+var version = 1,
+  circuitBreakers = {},
+  opt = {
+      errorThreshold: 50,
+      timeoutDuration: 2000,
+      volumeThrehold: 2,
+      windowDuraion: 1000
+  };
+
+self.addEventListener('install', function(evt) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(evt) {
+    if (self.clients && clients.claim) {
+        clients.claim();
+    }
+});
+
+CircuitBreaker.prototype.fetch = function(request) {
+    var unavailableRes = Response.error();
+
+    return new Promise(function(resolve, reject) {
+        this.run(function(success, fail) {
+            fetch(request).then(function(res) {
+                if(res.status < 400) {
+                    success();
+                    console.log('FETCH: successful');
+                } else {
+                    fail();
+                    console.log('FETCH: failed');
+                }
+                resolve(res);
+            }).catch(function(err) {
+                fail();
+                reject(unavailableRes);
+                console.log('FETCH: unavilable');
+            });
+        }, function() {
+            resolve(unavailableRes);
+        });
+    }.bind(this));
+};
+
+self.addEventListener('fetch', function(evt) {
+    var url = evt.request.url;
+
+    if(!circuitBreakers[url]) {
+        circuitBreakers[url] = new CircuitBreaker(opt);
+    }
+
+    evt.respondWith(circuitBreakers[url].fetch(evt.request));
+});

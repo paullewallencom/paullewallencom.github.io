586084de1402a76f115cc9c9843d681f7540d13c
Paul Lewallen <paul.lewallen@gmail.com>
2018-11-14 06:44:48 -0800

    038-Fetching-remote-resources

---
+++ 037-Working-with-Google-Analytics/index.js
@@ -0,0 +1,5 @@
+<!-- https://developers.google.com/web/showcase/2015/service-workers-iowa -->
+
+<!-- https://developers.google.com/web/updates/2016/07/offline-google-analytics?hl=en -->
+
+<!-- https://developers.google.com/web/tools/workbox/guides/enable-offline-analytics -->
